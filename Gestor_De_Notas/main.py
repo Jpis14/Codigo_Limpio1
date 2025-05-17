2e7e90ae2aa60437df3bd82a6f5ff1016db6c616
Jpis14 <89980336+Jpis14@users.noreply.github.com>
2025-05-17 18:56:45 -0500

    Update main.py

--- Gestor_De_Notas/main.py
+++ Gestor_De_Notas/main.py
@@ -1,6 +1,15 @@
 from UI.interfaz import iniciar_interfaz
+from Model.Start_Consola import Start_Consola
+
 
 if __name__ == "__main__":
-    iniciar_interfaz()
-
-
+    modo = input("¿Usar interfaz gráfica (g) o consola (c)? ").strip().lower()
+    if modo == 'g':
+        iniciar_interfaz()
+    elif modo == 'c':
+        #Inicio de Consola
+        if __name__ == "__main__":
+            start = Start_Consola()
+            start.start()
+    else:
+        print("Opción no válida.")
