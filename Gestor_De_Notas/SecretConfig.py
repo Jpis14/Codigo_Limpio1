1a47ff77d02a2b4ab058efb8f68483ce7b8469a2
Jpis14 <89980336+Jpis14@users.noreply.github.com>
2025-05-28 14:33:53 -0500

    Add files via upload

---
+++ Gestor_De_Notas/SecretConfig.py
@@ -0,0 +1,24 @@
+#AQUI VAN LOS DATOS DE TU DB
+#           |    |
+#           |    |
+#           |    |
+#           |    |
+#           |    |
+#       -----    -----
+#       \            /
+#        \          /
+#         \        /
+#          \      / 
+#           \    /
+#            \  /
+#             \/
+
+
+
+# Do not expose your Neon credentials to the browser
+
+PGHOST='ep-black-sun-a56lvi2v-pooler.us-east-2.aws.neon.tech'
+PGDATABASE='GESTORNOTASDB'
+PGUSER='GESTORNOTASDB_owner'
+PGPASSWORD='npg_P5FMYxO3iyZT'
+PGPORT=5432
